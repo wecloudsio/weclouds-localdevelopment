e8b52f199151071fe04a7d0d94442aeeed1bebbd
wecloudsio <wecloudsio@gmail.com>
2022-12-22 17:46:39 +0100

    init table for postgres: more attributes

--- local-development/init/postgres/postgres-init.sql
+++ local-development/init/postgres/postgres-init.sql
@@ -1,13 +1,23 @@
+-- Creation of user table
+CREATE TABLE IF NOT EXISTS "user"
+(
+    id                varchar(40) PRIMARY KEY,
+    version           integer,
+    token             varchar(5),
+    creation_date     timestamp   NOT NULL,
+    modification_date timestamp   NOT NULL,
+    username          varchar(20) NOT NULL
+);
+
+
 -- Creation of cloud table
 CREATE TABLE IF NOT EXISTS cloud
 (
-    id
-    uuid
-    NOT
-    NULL,
-    name
-    varchar
-(
-    20
-) NOT NULL
-    );
+    id                varchar(40) PRIMARY KEY,
+    version           integer,
+    token             varchar(5),
+    creation_date     timestamp   NOT NULL,
+    modification_date timestamp   NOT NULL,
+    owner_ref         varchar(40) NOT NULL,
+    name              varchar(20) NOT NULL
+);
